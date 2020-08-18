c4fb87b280afb8935b69473f5dff4b17fdcabaaa
Yuri <47905419+yurioliveira3@users.noreply.github.com>
2020-08-18 19:25:06 -0300

    Update convert_charset_to_UTF8.sh

--- convert_charset_to_UTF8.sh
+++ convert_charset_to_UTF8.sh
@@ -7,7 +7,7 @@ for file in "${files[@]}"; do
         echo "Ajustando o arquivo $file..."
         CHARSET="$(file -bi "$file"|awk -F "=" '{print $2}')"
         if [ "$CHARSET" != utf-8 ]; then
-                iconv -f "$CHARSET" -t utf8 "$file" -o "$file"
+                iconv -f "$CHARSET" -t utf8//TRANSLIT "$file" -o "$file"
         fi
 
         sed -i 's/^M$//' $file
