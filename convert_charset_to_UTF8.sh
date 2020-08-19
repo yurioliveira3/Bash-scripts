4c2633807cd7663bb4c86fdfd6ceee9280909e04
Yuri <47905419+yurioliveira3@users.noreply.github.com>
2020-08-19 15:53:32 -0300

    Update convert_charset_to_UTF8.sh

--- convert_charset_to_UTF8.sh
+++ convert_charset_to_UTF8.sh
@@ -4,14 +4,22 @@ files=( *.csv ) # printa todos os arquivos de extensão ".csv"
 
 # iterate over them
 for file in "${files[@]}"; do
-        echo "Ajustando o arquivo $file..."
+        echo "Fix $file..."
         CHARSET="$(file -bi "$file"|awk -F "=" '{print $2}')"
         if [ "$CHARSET" != utf-8 ]; then
-                iconv -f "$CHARSET" -t utf8//TRANSLIT "$file" -o "$file"
+                if [ "$CHARSET" = binary ]; then #Force if binary
+                        iconv -f iso-8859-14 -t utf8//TRANSLIT "$file" -o "$file.tmp" #file.tmp is to avoid bus erros
+                        mv $file.tmp $file
+                else 
+                        iconv -f "$CHARSET" -t utf8//TRANSLIT "$file" -o "$file.tmp" #file.tmp is to avoid bus erros
+                        mv $file.tmp $file
+                fi
         fi
-
-        sed -i 's/^M$//' $file
+        
+        #fix special characters
+        sed -i 's/^M$//' $file 
         sed -i '1s/^\xEF\xBB\xBF//' $file
 
-        echo $(file -i $file)
+        #print actual charset 
+        echo $(file -i $file) 
 done
