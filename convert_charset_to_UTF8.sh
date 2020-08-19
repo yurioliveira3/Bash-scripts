ed38f48ecf19f961585a57ba9275c30c28afab11
Yuri <47905419+yurioliveira3@users.noreply.github.com>
2020-08-19 17:34:55 -0300

    Update convert_charset_to_UTF8.sh

--- convert_charset_to_UTF8.sh
+++ convert_charset_to_UTF8.sh
@@ -1,25 +1,29 @@
 #!/bin/bash
 
+cd temppastadumparquivos/
+
 files=( *.csv ) # printa todos os arquivos de extensão ".csv"
 
 # iterate over them
 for file in "${files[@]}"; do
-        echo "Fix $file..."
+        echo "Fixing $file"
         CHARSET="$(file -bi "$file"|awk -F "=" '{print $2}')"
         if [ "$CHARSET" != utf-8 ]; then
                 if [ "$CHARSET" = binary ]; then #Force if binary
                         iconv -f iso-8859-14 -t utf8//TRANSLIT "$file" -o "$file.tmp" #file.tmp is to avoid bus erros
                         mv $file.tmp $file
-                else 
+                else
                         iconv -f "$CHARSET" -t utf8//TRANSLIT "$file" -o "$file.tmp" #file.tmp is to avoid bus erros
                         mv $file.tmp $file
                 fi
         fi
-        
+
         #fix special characters
-        sed -i 's/^M$//' $file 
+        sed -i 's/^M$//' $file
         sed -i '1s/^\xEF\xBB\xBF//' $file
 
-        #print actual charset 
-        echo $(file -i $file) 
+        #print actual charset
+        #echo $(file -i $file)
 done
+
+pv ../0-importar_arquivos.sql | mysql -u syntesisus -psyntesis#1#2 --default-character-set="utf8" topsapp
